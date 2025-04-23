84df1e9a17ddfd9740ab52472826483b88f06a08
1EMRE0 <ovacike7@gmail.com>
2025-04-23 15:12:25 +0300

    A simple note-taking app built with ASP.NET MVC and MSSQL
    A simple note-taking web application built with ASP.NET MVC, C#, and Entity Framework. Users can create, edit, delete, and view notes. MSSQL is used as the database.

---
+++ not_defteri/not_defteri/Migrations/20250418235217_yeni.cs
@@ -0,0 +1,28 @@
+﻿ using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace not_defteri.Migrations
+{
+    /// <inheritdoc />
+    public partial class yeni : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Context",
+                table: "Nots",
+                newName: "Content");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Nots",
+                newName: "Context");
+        }
+    }
+}
---
+++ not_defteri/not_defteri/Models/NotDefteriDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace not_defteri.Models
+{
+    public class NotDefteriDbContext : DbContext
+    {
+        public NotDefteriDbContext(DbContextOptions<NotDefteriDbContext> options) : base(options) 
+        { 
+        }
+        public DbSet <Not> Nots { get; set; }
+
+        //public DbSet<User> Users { get; set; }
+           
+    }
+}
